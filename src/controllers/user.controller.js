643d9232b2b549b85000c60d23526214a8a4aaff
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2024-07-02 18:27:31 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser=asyncHandler(async(req,res)=>{
     throw new apiError(400,"All fields are required");
 }
 
-const existedUser=User.findOne({
+const existedUser= await User.findOne({
     $or:[{email},{username}]
 })
 
@@ -31,10 +31,17 @@ if(existedUser){
 }
 
 const avatarLocalPath=req.files?.avatar[0].path;
-const coverImageLocalPath=req.files?.coverImage[0].path;
+// const coverImageLocalPath=req.files?.coverImage[0].path;
 
 if(!avatarLocalPath){
     throw new apiError(400,"avatar file is required");
+
+}
+ 
+
+  let coverImageLocalPath;
+if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length >0){
+    coverImageLocalPath=req.files.coverImage[0].path
 }
 
 const avatar= await uploadOnCloudinary(avatarLocalPath);
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary=async(localfilepath)=>{
     })
 
     // file has been uploaded successfully
-    console.log("file has been uploaded successfully",response.url)
+    // console.log("file has been uploaded successfully",response.url)
+    fs.unlinkSync(localfilepath)
     return response;
     } catch(error) {
       fs.unlinkSync(localfilepath);
